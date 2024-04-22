06e33b6ae0a33b707ffbdf6311a9c9dce34b904e
SimonaStoeva <161055613+SimonaStoeva@users.noreply.github.com>
2024-04-22 20:57:50 +0300

    Add files via upload

---
+++ Operation.java
@@ -0,0 +1,9 @@
+package oop1.svg.f22621604;
+
+public interface Operation {
+    void print();
+    void create();
+    void erase(int n);
+    void translate(int x, int y);
+    void within(String option);
+}
