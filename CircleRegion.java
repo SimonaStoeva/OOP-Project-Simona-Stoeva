d540739227ae6ee2ac4f1c00740b89622e5fa33f
SimonaStoeva <161055613+SimonaStoeva@users.noreply.github.com>
2024-05-05 23:16:28 +0300

    Add files via upload

--- CircleRegion.java
+++ CircleRegion.java
@@ -1,18 +1,45 @@
 package oop1.svg.f22621604.figures;
+import oop1.svg.f22621604.contracts.Region;
 
-public class CircleRegion extends Circle{
+public class CircleRegion implements Region {
+    private int cx;
+    private int cy;
+    private int radius;
 
-    public CircleRegion(int cx, int cy, int radius, String color) {
-        super(cx, cy, radius, color);
+    public CircleRegion(int cx, int cy, int radius) {
+        this.cx = cx;
+        this.cy = cy;
+        this.radius = radius;
     }
 
-    public boolean contains(Circle circle){
-        double circleCx=circle.getCx();
-        double circleCy=circle.getCy();
-        double circleRadius=circle.getRadius();
+    @Override
+    public boolean checkRectangle(int otherX, int otherY, int otherWidth, int otherHeight) {
+        int distanceX=Math.abs(cx-(otherX+otherWidth/2));
+        int distanceY=Math.abs(cy-(otherY+otherWidth/2));
 
-        double distance=Math.sqrt(Math.pow(circleCx - getCx(), 2) + Math.pow(circleCy - getCy(), 2));
-         return distance+circleRadius <= getRadius();
+        if(distanceX>(otherWidth/2+radius)){
+            return false;
+        }
 
+        if(distanceY>(otherHeight/2+radius)){
+            return false;
+        }
+
+        if(distanceX<=(otherWidth/2)){
+            return true;
+        }
+
+        if(distanceY<=(otherHeight/2)){
+            return true;
+        }
+
+        double cornerDistance=Math.pow(distanceX-otherWidth/2,2)+Math.pow(distanceY-otherHeight/2,2);
+        return cornerDistance<=Math.pow(radius,2);
+    }
+
+    @Override
+    public boolean checkCircle(int otherCx, int otherCy, int otherRadius) {
+        double distance = Math.sqrt(Math.pow(otherCx - cx, 2) + Math.pow(otherCy - cy, 2));
+        return distance+otherRadius<=radius;
     }
 }
--- RectangleRegion.java
+++ RectangleRegion.java
@@ -1,18 +1,48 @@
 package oop1.svg.f22621604.figures;
+import oop1.svg.f22621604.contracts.Region;
 
-public class RectangleRegion extends Rectangle {
-    public RectangleRegion(int x, int y, int width, int height, String color) {
-        super(x, y, width, height, color);
+public class RectangleRegion implements Region {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    public RectangleRegion(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
     }
 
-    public boolean contains(Rectangle rectangle){
-        int rectX= rectangle.getX();
-        int rectY=rectangle.getY();
-        int rectWidth=rectangle.getWidth();
-        int rectHeight=rectangle.getHeight();
+    @Override
+    public boolean checkRectangle(int otherX, int otherY, int otherWidth, int otherHeight) {
+        return otherX>=x && otherY>=y && otherX+otherWidth<=x+width
+                && otherY+otherHeight<y+height;
+    }
 
-        return rectX >= getX() && rectY >= getY() &&
-                rectX+rectWidth <= getX()+getWidth() &&
-                rectY+rectHeight <= getY()+getHeight();
+    @Override
+    public boolean checkCircle(int otherCx, int otherCy, int otherRadius) {
+        int rectCenterX = x + width / 2;
+        int rectCenterY = y + height / 2;
+
+        int circleDistanceX = Math.abs(otherCx - rectCenterX);
+        int circleDistanceY = Math.abs(otherCy - rectCenterY);
+
+        if (circleDistanceX > (width / 2 + otherRadius)) {
+            return false;
+        }
+        if (circleDistanceY > (height / 2 + otherRadius)) {
+            return false;
+        }
+
+        if (circleDistanceX <= (width / 2)) {
+            return true;
+        }
+        if (circleDistanceY <= (height / 2)) {
+            return true;
+        }
+
+        double cornerDistanceSq = Math.pow(circleDistanceX - width / 2, 2) + Math.pow(circleDistanceY - height / 2, 2);
+        return cornerDistanceSq <= Math.pow(otherRadius, 2);
     }
 }
